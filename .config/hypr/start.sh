4af4ef502bbc59e3592f3c29d46dbaeb6015bc58
Innovator-super <alexeyinnovator@yandex.ru>
2025-07-26 23:41:44 +0300

    first commit

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -2,7 +2,7 @@
 
 swww init &
 
-swww img ~/Images/Wallpaper/dhxnx5k-35a0b7f5-9360-45d3-b061-458eed9bbc6.png &
+swww img /home/alex/Images/Wallpaper/dhxnx5k-35a0b7f5-9360-45d3-b061-458eed9bbc6.png &
 
 nm-applet --indicator &
 
